896507840740dabd78b877dde5626b88833313eb
ErichLeonardo <erich.hamm21@gmail.com>
2024-03-09 17:19:52 +0100

    Cambio ardilla_poema

--- src/main/java/org/Tortugas/model/ardilla_poema.java
+++ src/main/java/org/Tortugas/model/ardilla_poema.java
@@ -1,15 +1,13 @@
 package org.Tortugas.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "ardilla_poema")
 public class ardilla_poema {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne
     private Ardilla ardilla;
